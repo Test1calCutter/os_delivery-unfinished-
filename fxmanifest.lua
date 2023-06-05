597189396a5f3ec627389e3a0e9e43101bf7a6f3
Noel <108223454+Test1calCutter@users.noreply.github.com>
2023-06-05 14:12:48 -0700

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+games { 'gta5' }
+dependency 'es_extended'
+
+author 'OnlyScripts / VeryAppropriateName#2324'
+description 'Delivery'
+version '1.0.0'
+
+client_script 'client.lua'
+server_script 'server.lua'
